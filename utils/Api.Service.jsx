0bab0de926d65e3ddff6e5fb12f7e23250bf1ad6
praveenrames <praveenrameshr2002@gmail.com>
2024-08-16 11:36:18 +0530

    First Commit

---
+++ utils/Api.Service.jsx
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const AxiosService = axios.create({
+  baseURL:'http://localhost:5000',
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+AxiosService.interceptors.request.use((config) => {
+  const token = sessionStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+export default AxiosService;
